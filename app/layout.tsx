809b06eac6389719f7f0a9a79b05bde56cc9ab71
abel254 <abelmuema353@gmail.com>
2023-07-22 23:54:38 +0300

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
+import { Abhaya_Libre } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
+// const abhaya_Libre = Abhaya_Libre({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-navcolor">
+      <nav className="flex justify-between items-center mx-16">
+        <div>
+          <h1 className="py-4">
+            <a href="#" className="font-bold font-montserrat-800 text-3xl text-white">
+              abel<span className="font-normal">muema.</span>
+            </a>
+          </h1>
+        </div>
+        <ul className="flex">
+          <li className="ml-1 font-semibold text-white">
+            <a href="#">
+              <span>Home</span>
+            </a>
+          </li>
+          <li className="ml-6 font-semibold text-white">
+            <a href="#">
+              <span>Skills</span>
+            </a>
+          </li>
+          <li  className="ml-6 font-semibold text-white">
+            <a href="#">
+              <span>Projects</span>
+            </a>
+          </li>
+          <li  className="ml-6 font-semibold text-white">
+           <a href="#" className="border-white border px-2 py-1.5 rounded">Let's Connect</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
